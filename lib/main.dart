66e41addeff6ef06edd38f0ebf3ce0e2d6d3fcfb
Muhammad Qaasim <q.akorede@featuremind.com>
2025-01-05 23:00:42 +0100

    feat: setup/configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
