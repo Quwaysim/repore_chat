c0dbbcfde6bf71f31e683e6db4331bec05d8106a
Muhammad Qaasim <q.akorede@featuremind.com>
2025-01-06 07:34:52 +0100

    feat: added assets and more colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color primary = Color(0xFF101828);
+  static const Color adminColor = Color(0xFF6941CB);
+  static const Color customerColor = Color(0xFFEAECF0);
+  static const Color agentColor = Color(0xFF0071BC);
   static const Color color0071BC = Color(0xFF0071BC);
+  static const Color color00B4D8 = Color(0xFF00B4D8);
+  static const Color color475467 = Color(0xFF475467);
 }
---
+++ lib/utils/asset_paths.dart
@@ -0,0 +1,10 @@
+class AssetPaths {
+  static const _svgDir = 'assets/svgs';
+  static const String icBack = '$_svgDir/ic_back.svg';
+  static const String icWaiting = '$_svgDir/ic_waiting.svg';
+  static const String icSent = '$_svgDir/ic_sent.svg';
+  static const String icFailed = '$_svgDir/ic_failed.svg';
+  static const String icDelivered = '$_svgDir/ic_delivered.svg';
+  static const String icSend = '$_svgDir/ic_send.svg';
+  static const String icUploadMedia = '$_svgDir/ic_upload_media.svg';
+}
